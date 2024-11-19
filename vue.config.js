2e3a09e157f1146aaa49126efb6c26e6e6b0f1c7
Jhony benitez <jonathanbenitez_98@hotmail.com>
2024-11-19 09:16:18 -0300

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/Semana7_tarea_vue'
+  ? '/Gestor-de-tareas-planillas-principal/'
   :'/'
 });
